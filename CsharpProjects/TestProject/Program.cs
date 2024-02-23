1901379ce426671b0394f781d5058cdd484173d1
David Berry <dberry3755@gmail.com>
2024-02-23 12:30:42 -0500

    fizz buzz

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -84,3 +84,16 @@ switch (product[2])
 }
 
 Console.WriteLine($"Product: {size} {color} {type}");
+
+
+for (int i = 1; i < 101; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+        Console.WriteLine($"{i} - FizzBuzz");
+    else if (i % 3 == 0)
+        Console.WriteLine($"{i} - Fizz");
+    else if (i % 5 == 0)
+        Console.WriteLine($"{i} - Buzz");
+    else
+        Console.WriteLine($"{i}");
+}
