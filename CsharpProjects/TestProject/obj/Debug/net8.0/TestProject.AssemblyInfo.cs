4f3089773f36a233ade1737ed0b9af7d8d12608c
David Berry <dberry3755@gmail.com>
2024-02-23 08:52:50 -0500

    coin flip ternary

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e380a71dcaf26c4bbe4000c87c707ea73bd58b2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a5c560fd057f6e839b2f5f2b030674c2b3880c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
