171157b5150ec8fbcfc21fb39c9397edaae0db3c
David Berry <dberry3755@gmail.com>
2024-02-23 14:06:25 -0500

    reverse array

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -271,3 +271,26 @@ Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {r
 
 float result3 = value3 / value1;
 Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+
+
+//Reverse letters in a sentence
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+// Step 1
+string[] message = pangram.Split(' ');
+
+//Step 2
+string[] newMessage = new string[message.Length];
+
+// Step 3
+for (int i = 0; i < message.Length; i++)
+{
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    newMessage[i] = new string(letters);
+}
+
+//Step 4
+string result = String.Join(" ", newMessage);
+Console.WriteLine(result);
